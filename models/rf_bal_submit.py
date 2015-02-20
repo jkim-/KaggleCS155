8ffdf99ceed596e46701422b80f4e2e67fac68f7
jkim <jaehongkim86@gmail.com>
2015-02-19 16:32:33 -0800

    Added weight balanced random forest

--- models/rf_bal_submit.py
+++ models/rf_bal_submit.py
@@ -11,8 +11,8 @@ import itertools
 import getpass
 
 param_list = [
-    [int(1.5**x) for x in range(1,15)], # n_estimators
-    [1, 2, 3, 4, 5, 7, 10, 20] # max_depth 
+    [256, 512, 1024], # n_estimators 
+    [int(1.5**x * np.sqrt(500)) for x in range(-5,7)] # max_features
 ]
 
 for i, pars in enumerate(itertools.product(*param_list)):
@@ -21,12 +21,12 @@ for i, pars in enumerate(itertools.product(*param_list)):
     # n_estimators
     arguments.append(str(pars[0]))
 
-    # max_depth
+    # max_features
     arguments.append(str(pars[1]))
 
     # model_fname
     arguments.append(
-        '/nfs/raid13/babar/dchao/KaggleCS155/models/bdt_std/{0}.pkl'.format(i)
+        '/nfs/raid13/babar/dchao/KaggleCS155/models/rf_bal/{0}.pkl'.format(i)
     )
 
     submit_file = tempfile.NamedTemporaryFile(mode='w+t', suffix='.submit')
@@ -34,12 +34,12 @@ for i, pars in enumerate(itertools.product(*param_list)):
     try:
         print 'Creating temporary submission file...'
         submit_file.writelines(['universe = vanilla\n',
-                                'executable = bdt_std.py\n',
+                                'executable = rf_bal.py\n',
                                 'getenv = True\n',
                                 'arguments = {0}\n'.format(' '.join(arguments)),
-                                'output = '+model_path+'/models/logs/bdt_std_'+str(i)+'.out\n',
-                                'error = '+model_path+'/models/logs/bdt_std_'+str(i)+'.error\n',
-                                'log = '+model_path+'/models/logs/bdt_std_'+str(i)+'.log\n',
+                                'output = '+model_path+'/models/logs/rf_bal_'+str(i)+'.out\n',
+                                'error = '+model_path+'/models/logs/rf_bal_'+str(i)+'.error\n',
+                                'log = '+model_path+'/models/logs/rf_bal_'+str(i)+'.log\n',
                                 'accounting_group = group_babar\n',
                                 'accounting_group_user = {0}\n'.format(getpass.getuser()),
                                 'queue'])
